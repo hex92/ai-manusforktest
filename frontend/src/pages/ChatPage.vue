d4f874d05175527462a8bffd4c99b15e4e1fa99c
Simpleyyt <simpleyyt@gmail.com>
2025-08-27 01:56:17 +0800

    Revert "fix: clear unread not working"
    This reverts commit 9ced929e1b527939f1696c6a680aa687bb8c6cb4.

--- frontend/src/pages/ChatPage.vue
+++ frontend/src/pages/ChatPage.vue
@@ -375,7 +375,9 @@ const restoreSession = async () => {
     handleEvent(event);
   }
   realTime.value = true;
-  await chat();
+  if (session.status === SessionStatus.RUNNING || session.status === SessionStatus.PENDING) {
+    await chat();
+  }
 }
 
 
